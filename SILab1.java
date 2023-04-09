0356e8037c4f56f108be5b6bf0018bbcef34357c
Vasko Pavic <vaskopavic@gmail.com>
2023-04-09 17:32:07 +0200

    Fix method sumEvenNumbers

--- SILab1.java
+++ SILab1.java
@@ -16,7 +16,7 @@ public class SILab1 {
     private static int sumEvenNumbers(List<Integer> list) {
         int sum = 1;
         for (Integer el : list) {
-            if (el % 2 == 0) {
+            if (el % 2 == 0 && i != 0) {
                 sum *= el;
             }
         }
